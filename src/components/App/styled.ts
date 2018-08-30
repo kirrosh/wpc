057a2033ddd9e92519d08939c4dac731bfd46ca8
Koshelev Kirill <olegartor7@gmail.com>
2018-08-30 19:03:27 +0300

    fix body style

--- src/components/App/styled.ts
+++ src/components/App/styled.ts
@@ -7,8 +7,4 @@ export const AppLayout = styled.div`
   color: ${({ theme }) => theme.colors.main};
   background-color: ${({ theme }) => theme.colors.background.light};
   width: 100%;
-  min-height: 1280px;
-  body {
-    margin: 0px;
-  }
 `;
