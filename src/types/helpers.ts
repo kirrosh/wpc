f33632ec5b7396184845da25b12496c50030afaa
Kirill Koshelev <k.koshelev@qasl.ru>
2018-12-27 15:45:19 +0300

    fix store

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,9 +1,13 @@
+import { Reducer } from 'react';
+
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
 
 type Signature<T> = T extends (...args: infer R) => any ? (...args: R) => void : any;
 
 export type DispatchPropsSignature<T> = Record<keyof T, Signature<T[keyof T]>>;
+
+export type ReducerState<T> = T extends Reducer<infer S, any> ? S : any;
 
 // can be useful in future
 // export type Thunk<R = void> = ThunkAction<R, IRootStore, void, Action>;
