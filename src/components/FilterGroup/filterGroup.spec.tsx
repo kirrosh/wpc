64a7db6351495a4d24aa238b2ffe1ece689b2faa
Kirill Koshelev <k.koshelev@qasl.ru>
2019-01-22 16:07:36 +0300

    filterGroup action test

--- src/components/FilterGroup/filterGroup.spec.tsx
+++ src/components/FilterGroup/filterGroup.spec.tsx
@@ -5,6 +5,7 @@ import FilterGroupMode from './FilterGroupMode';
 import { FilterGroupModeProps } from './types';
 import Button from 'common/Button';
 import Select from 'common/Select';
+import MinMax from 'common/MinMax';
 
 test('filter group mode', () => {
   const MODE: FilterGroupModeProps = {
@@ -25,5 +26,29 @@ test('filter group mode', () => {
   expect(element.find(StyledFilterGroupMode)).toHaveLength(1);
   expect(element.find(Select)).toHaveLength(1);
   expect(element.find(Button)).toHaveLength(1);
+  expect(element.find(MinMax)).toHaveLength(1);
 });
 
+test('remove filter group mode', () => {
+  const MODE: FilterGroupModeProps = {
+    modeId: 'id',
+    modeValues: {
+      name: 'name',
+      values: {
+        max: 'max',
+        min: 'min',
+      },
+    },
+    onModeRemove: () => {},
+    onModeValuesChange: () => {},
+  };
+  const onModeRemove = jest.fn();
+  const onModeValuesChange = jest.fn();
+
+
+  const element = shallow(<FilterGroupMode {...MODE} onModeRemove={onModeRemove} onModeValuesChange={onModeValuesChange}/>);
+
+  const btn = element.find(Button);
+  btn.simulate('click');
+  expect(onModeRemove.mock.calls.length).toBe(1);
+});
